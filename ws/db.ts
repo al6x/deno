d16ac3967297c64479ee4e2ad0951980898ef914
alexeypetrushin <alexey.petrushin@gmail.com>
2021-05-21 12:07:17 +0300

    upd

--- ws/db.ts
+++ ws/db.ts
@@ -2,12 +2,21 @@ import { p, something } from "base/base.ts"
 import { Log } from "base/log.ts"
 import { Client, Pool, PoolClient } from "postgres/mod.ts"
 
+// Hiding notice warnings, should be removed when issue fixed
+// https://github.com/denodrivers/postgres/issues/254
+import { parseNotice } from "postgres/connection/warning.ts"
+import { Connection } from "postgres/connection/connection.ts"
+
+(Connection.prototype as something).processNotice = function(msg: something): something {
+  return parseNotice(msg)
+}
+
 
 // Db ----------------------------------------------------------------------------------------------
 export class Db {
-  private readonly pool: Pool
-  private readonly log:  Log
-  public  readonly url:  PgUrl
+  private          pool?: Pool
+  private readonly log:   Log
+  public  readonly url:   PgUrl
 
   constructor(
     public readonly nameOrUrl: string,
@@ -15,24 +24,32 @@ export class Db {
   ) {
     this.url = parsePgUrl(nameOrUrl)
     this.log = new Log("Db", this.url.name)
-    this.pool = new Pool({
-      hostname: this.url.host,
-      port:     this.url.port,
-      user:     this.url.user,
-      password: this.url.password,
-      database: this.url.name
-    }, poolSize, true)
   }
 
   // async connect(): Promise<void> {
   //   await this.pool.connect()
   // }
 
+  private getPool() {
+    if (this.pool == undefined) {
+      this.log.info("connect")
+      this.pool = new Pool({
+        hostname: this.url.host,
+        port:     this.url.port,
+        user:     this.url.user,
+        password: this.url.password,
+        database: this.url.name
+      }, this.poolSize, true)
+    }
+    return this.pool
+  }
+
   async exec<T = unknown[]>(sql: string, params: something[] = []): Promise<T[]> {
     this.log.with({ sql: sql }).info("exec")
     let conn: PoolClient | undefined = undefined
+    let pool = this.getPool()
     try {
-      conn = await this.pool.connect()
+      conn = await pool.connect()
       if (params.length == 0) {
         let { rows } = await conn.queryArray(sql)
         return (rows as something)
@@ -40,10 +57,14 @@ export class Db {
         let { rows } = await conn.queryArray({ text: sql, args: params })
         return (rows as something)
       }
-    // } catch(e) {
-    //   // Quitting on any db error
-    //   this.log.with({ sql: sql }).with(e).error("can't execute, quitting")
-    //   Deno.exit(1)
+    } catch(e) {
+      this.log.with({ sql: sql }).with(e).error("can't execute, quitting")
+      // Terminating pool on any error, it will be re-connected on demand
+      this.log.info("disconnect")
+      if (this.pool == pool) this.pool = undefined
+      try   { await pool.end() }
+      catch {}
+      throw e
     } finally {
       await conn?.release()
     }
